aeb6e797e63ad08a2723f3bcdb2d8afac509391e
msilvaa <msilvaa@unal.edu.co>
2019-10-02 08:59:29 -0500

    scope

---
+++ 2019-09-11-variables/scope.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+ int main(void)
+{
+  double miles = 0.0;
+  double km = 0.0;
+  std:: cout << "escribge el numero de millas :\n";
+  std:: cin >>miles;
+  km = miles*1.609;
+    std::cout <<km <<"\n";
+  return 0;
+  
+}
